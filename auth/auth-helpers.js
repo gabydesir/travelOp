41f59fc66156dccb874702782784e06bde4ff7f5
gaby desir <gabrielle@gwu.edu>
2017-10-11 11:36:26 -0400

    completed auth set up

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -23,3 +23,16 @@ function createNewUser(req, res) {
   });
 }
 
+function loginRequired(req, res, next){
+  if (!req.user) res.redirect('/auth/login');
+
+  return next();
+}
+
+module.exports = {
+  comparePass,
+  loginRedirect,
+  loginRequired,
+  createNewUser,
+};
+
