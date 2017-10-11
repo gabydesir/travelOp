a998a77eaf3bbcf2efcb4a3a1f3ea901f805d143
gaby desir <gabrielle@gwu.edu>
2017-10-11 10:25:19 -0400

    set up auth route, and auth helpers

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -4,3 +4,22 @@ const User = require('../models/user');
 function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
+
+function loginRedirect(req, res, next) {
+  if (req.user) res.redirect('/user');
+
+  return next();
+}
+
+function createNewUser(req, res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  return User.create({
+    username: req.body.username,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: hash,
+  });
+}
+
